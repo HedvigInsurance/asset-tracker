5101b507e886b04ee7e62a8663b8d10c06d97f26
Johan Tjelldén <johan@hedvig.com>
2018-01-12 10:46:52 +0100

    Enteties should handle large files

--- src/main/java/com/hedvig/generic/mustrename/query/UploadFile.java
+++ src/main/java/com/hedvig/generic/mustrename/query/UploadFile.java
@@ -2,12 +2,8 @@ package com.hedvig.generic.mustrename.query;
 
 import java.util.UUID;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
- 
+import javax.persistence.*;
+
 @Entity
 @Table(name = "file_uploads")
 public class UploadFile {
@@ -64,7 +60,8 @@ public class UploadFile {
     public void setImageId(UUID image_id) {
         this.imageId = image_id;
     }
-    
+
+    @Lob
     @Column(name = "data")
     public byte[] getData() {
         return data;
